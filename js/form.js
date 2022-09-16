f70f251aff9939d7d74e47ea452c0cd88ced4eb5
Gumidafe Socorro <devgumidafe@gmail.com>
2022-09-16 11:23:12 +0100

    edit contact.html

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("#form");
+const form = document.querySelector('#form');
 
 const expressions = {
   name: /^[a-zA-Z0-9À-ÿ\s]{2,40}$/, // Letras y espacios, pueden llevar acentos.
@@ -7,13 +7,13 @@ const expressions = {
 };
 
 const errorMessages = {
-  name: "El nombre tiene que contener de 2 a 40 caracteres y solo admite caracteres validos",
-  email: "El email no es válido, ejemplo@email.com",
+  name: 'El nombre tiene que contener de 2 a 40 caracteres y solo admite caracteres validos',
+  email: 'El email no es válido, ejemplo@email.com',
   message:
-    "El mensaje tiene que contener de 10 a 500 caracteres y solo admite caracteres validos",
+    'El mensaje tiene que contener de 10 a 500 caracteres y solo admite caracteres validos',
 };
 
-const fieldList = ["name", "email", "message"];
+const fieldList = ['name', 'email', 'message'];
 
 const validateForm = (fieldList) => {
   let isValid = true;
@@ -22,7 +22,7 @@ const validateForm = (fieldList) => {
     const value = document.getElementById(field).value.trim();
 
     if (expressions[field].test(value)) {
-      offError(field, "");
+      offError(field, '');
     } else {
       isValid = false;
       onError(field, errorMessages[field]);
@@ -37,12 +37,12 @@ const onError = (id, message) => {
   const messageElement = document.getElementById(`${id}-error`);
 
   if (element) {
-    element.classList.add("error");
+    element.classList.add('error');
   }
 
   if (messageElement) {
     messageElement.textContent = message;
-    messageElement.classList.add("error-message");
+    messageElement.classList.add('error-message');
   }
 };
 
@@ -51,18 +51,18 @@ const offError = (id, message) => {
   const messageElement = document.getElementById(`${id}-error`);
 
   if (element) {
-    element.classList.remove("error");
+    element.classList.remove('error');
   }
 
   if (messageElement) {
     messageElement.textContent = message;
-    messageElement.classList.remove("error-message");
+    messageElement.classList.remove('error-message');
   }
 };
 
 const sendEmail = (formData) => {
-  fetch("phpMailer/email.php", {
-    method: "POST",
+  fetch('phpMailer/email.php', {
+    method: 'POST',
     body: formData,
   })
     .then((res) => res.json())
@@ -72,26 +72,25 @@ const sendEmail = (formData) => {
 };
 
 const showAlert = (res) => {
-  if (res === "ok") {
+  if (res === 'ok') {
     Swal.fire({
-      title: "Exito!",
-      text: "Mensaje enviado",
-      icon: "success",
+      title: 'Genial!',
+      text: 'Mensaje enviado correctamente',
+      icon: 'success',
     });
-
     form.reset();
   } else {
     Swal.fire({
-      title: "Oops!",
-      text: "Error al enviar el mensaje",
-      icon: "error",
+      title: 'Oops!',
+      text: 'Error al enviar el mensaje',
+      icon: 'error',
       footer:
-        "Inténtelo de nuevo, si el problema persiste contacte en devgumidafe@gmail.com",
+        'Inténtelo de nuevo, si el problema persiste contacte en devgumidafe@gmail.com',
     });
   }
 };
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const formData = new FormData(form);
